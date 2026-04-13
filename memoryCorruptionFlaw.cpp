dbae641b839494a3d67c4aeaa744bcf3e74a6e78
LiamTrodden1 <liamtrodden28@gmail.com>
2026-04-13 12:20:33 +0100

    Task C

---
+++ memoryCorruptionFlaw.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+// temp = a stored integer that the buffer overflow will leak into causing the value to change
+// type a string longer than 8 letters to cause buffer overflow
+
+// check users password whne they log in
+void login() {
+    int temp = 1;
+    char password[8];
+    
+    cout << "Enter password: ";
+    cin >> password; 
+
+    // check if temp has changed (buffer overflow occured)
+    if (temp != 1) {
+        cout << "buffer overflow occured\n";
+        cout << "current temp value is: " << temp;
+    }
+    // password correct
+    else if (strcmp(password, "password") == 0) {
+        cout << "Password Correct";
+    }
+    // password incorrect
+    else {
+        cout << "Password Incorrect";
+    }
+}
+
+int main() {
+    login();
+}
